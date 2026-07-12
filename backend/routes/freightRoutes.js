c2e49fac67b1038445ca2df02c18a9b09e39b887
رضا قزل باش <rghezelbash.68@gmail.com>
2026-07-12 09:39:48 +0330

    هضلاح فیلد مقصد کارشناس فروش

--- backend/routes/freightRoutes.js
+++ backend/routes/freightRoutes.js
@@ -97,7 +97,7 @@ router.get('/line-analytics', authenticateToken, getLineAnalytics);
 router.get(
   '/routes/search',
   authenticateToken,
-  authorizeRole(['planner', 'planner_manager', 'transport_user', 'personal_transport_user', 'transport_finance', 'finance', 'admin']),
+  authorizeRole(['planner', 'planner_manager', 'sales_expert', 'transport_user', 'personal_transport_user', 'transport_finance', 'finance', 'admin']),
   searchDispatchRoutes
 );
 
